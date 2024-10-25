f45d8d5338bfd9982de4d87dd64cfab875026724
smcoderr <souravoz2018@gmail.com>
2024-10-25 19:09:59 +0530

    localhost to 0.0.0.0 in listen

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const server = createServer((req, res) => {
 const PORT =process.env.PORT || 3000
 
 // starts a simple http server locally on port 3000
-server.listen(PORT, '127.0.0.1', () => {
-  console.log('Listening on 127.0.0.1:3000');
+server.listen(PORT, '0.0.0.0', () => {
+  console.log('Listening on 0.0.0.0:3000');
 });
 
 // run with `node server.mjs`
